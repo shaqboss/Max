be7485841043fe4ff15194a890dcf20f6a954b15
Python Master <83932005+shaqboss@users.noreply.github.com>
2021-08-16 21:31:56 +0000

    Bot 2.2

--- main.py
+++ main.py
@@ -7,30 +7,53 @@ bot = commands.Bot(command_prefix='>')
 
 @bot.event
 async def on_connect():
-    print("Max Ai System online")
+    print("Max Ai System online 😀")
 
 
 @bot.command()
 async def start(ctx):
-    await ctx.send("Hello Am Max Ai System")
+    await ctx.send("Hello Am Max Ai System 🧠")
 
 
 @bot.command()
 async def Max(ctx):
-    await ctx.send("What may I do for you master")
+    await ctx.send("What may I do for you master 👑")
 
     @bot.command()
     async def Max_Greet(ctx):
         await ctx.send(
-            "Welcome To our Group Am Max The Groups Ai System am Made By Shaquan Ceres user name: Shaquanceres#5976"
+            "🤖 Welcome To our Group Am Max The Groups Ai System am Made By Shaquan Ceres user name: Shaquanceres#5976"
         )
 
 
 @bot.command()
 async def Max_Bye(ctx):
-    await ctx.send("Bye thank for being apart of our group")
+    await ctx.send("Bye thank for being apart of our group 🤧")
+
+
+@bot.command()
+async def Max_Say_Hello(ctx):
+    await ctx.send("Hello am Max Whats up 🥰")
+
+
+@bot.command()
+async def Max_How_Too_Make_A_Bot_Like_You(ctx):
+    await ctx.send(
+        "Go too https://replit.com/@Shaquanceres/Max#main.py too get my coding program "
+    )
+
+
+@bot.command()
+async def Max_How_Are_You(ctx):
+    await ctx.send("Am Good and fully runing I hope you are to 😁 😘")
+
+
+@bot.command()
+async def Max_Say_Hello_World(ctx):
+    await ctx.send("Hello World 🌎")
 
 
 my_secret = os.environ['Token']
 
 bot.run(my_secret)
+
